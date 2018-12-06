cc15c121513a82f06f768fba91b81435ea46166d
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-06 22:25:57 +0100

    addedItems

--- startPage.js
+++ startPage.js
@@ -1,9 +1,10 @@
 
-//fix navbar bootstrap
-//fix footer 
+//fix navbar bootstrap 
 //fix section
 
 function init(){
+    //This is an asyncron function which means that it will pull the download
+    //prccoess beside to download and go on 
     var rugs = undefined;
     fetch("rugs.json")
     .then(function(getAllInfo){
@@ -18,16 +19,54 @@ function init(){
 }
 
 function createMainDiv(rugs){
-    var div = document.createElement("div");
-    div.classList.add("mainDiv")
 
-    for(var i = 0; i <rugs.length; i++){
-        var divForSingleProduct = document.createElement("div");
-        divForSingleProduct.classList.add("singleDiv")
-        console.log(divForSingleProduct)
-        //divForSingleProduct.appendChild(rugs[i])
-        div.appendChild(divForSingleProduct);
-    }
     var section = document.querySelector("section");
-    section.appendChild(div)
+   
+    for(var i = 0; i <rugs.length; i++){
+
+        var divForSingleProduct = createSingleDiv();
+        divForSingleProduct.appendChild(createTitle(rugs[i]))
+        divForSingleProduct.appendChild(createMade(rugs[i]))
+        divForSingleProduct.appendChild(createYear(rugs[i]))
+        divForSingleProduct.appendChild(createColor(rugs[i]))
+        divForSingleProduct.appendChild(createImg(rugs[i]))
+        divForSingleProduct.appendChild(createPrice(rugs[i]))
+        section.appendChild(divForSingleProduct)
+    }
+}
+
+function createSingleDiv(){
+    var singlDiv = document.createElement("div");
+    return singlDiv
+    
+}
+function createTitle(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.title;
+    return h4
+}
+function createMade(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.made;
+    return h4
+}
+function createYear(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.year;
+    return h4
+}
+function createColor(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.color;
+    return h4
+}
+function createImg(infoAboutProduct){
+    var img = document.createElement("img")
+    img.src = infoAboutProduct.image;
+    return img
+}
+function createPrice(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.price;
+    return h4
 }
