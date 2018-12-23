dca284377df6b091d48800aeea0837e6bcf8a4b9
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-23 14:39:00 +0100

    getProToKundvagn

--- kundVagn.js
+++ kundVagn.js
@@ -1,6 +1,80 @@
 function init(){
-var product = localStorage.getObject("shoppingCart")
-var section = document.querySelector("section");
 
+    var rugs = localStorage.getObject("shoppingCart");
+    createMainDiv(rugs)
 }
+
+
+function createMainDiv(rugs){
+
+    var section = document.querySelector("section");
+   
+    for(var i = 0; i <rugs.length; i++){
+
+        var divForSingleProduct = createDiv();
+        divForSingleProduct.appendChild(createTitle(rugs[i]))
+        divForSingleProduct.appendChild(createMade(rugs[i]))
+        divForSingleProduct.appendChild(createYear(rugs[i]))
+        divForSingleProduct.appendChild(createColor(rugs[i]))
+        divForSingleProduct.appendChild(createImg(rugs[i]))
+        divForSingleProduct.appendChild(createPrice(rugs[i]))
+        divForSingleProduct.appendChild(createDeleteButton(rugs))
+        section.appendChild(divForSingleProduct)
+    }
+}
+
+function createDiv(){
+    var singlDiv = document.createElement("div");
+    singlDiv.classList.add("singleDivDesign")
+    return singlDiv
     
+}
+function createTitle(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.title;
+
+    return h4
+}
+function createMade(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.made;
+    return h4
+}
+function createYear(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.year;
+    return h4
+}
+function createColor(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.color;
+    return h4
+}
+function createImg(infoAboutProduct){
+    var img = document.createElement("img")
+    img.classList.add("imgProduct")
+    img.src = infoAboutProduct.image;
+    return img
+}
+function createPrice(infoAboutProduct){
+    var h4 = document.createElement("h4")
+    h4.innerText = infoAboutProduct.price +"kr";
+    return h4
+}
+function createDeleteButton(infoAboutProduct){
+    var deleteButton = document.createElement("button");
+    deleteButton.innerText = "Ta bort product";
+    deleteButton.onclick = function(){
+    for()
+
+
+
+
+
+    var shoppingCartParsed = localStorage.getObject("shoppingCart");
+    var i = document.querySelector("i.fa-shopping-cart");
+    i.innerText =  shoppingCartParsed.length;
+    
+    }
+    return deleteButton
+}
