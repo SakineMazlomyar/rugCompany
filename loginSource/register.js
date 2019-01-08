c86655f48eb7d0ef313f3aeed531e32046e79697
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-08 16:33:12 +0100

    addedRej

---
+++ loginSource/register.js
@@ -0,0 +1,9 @@
+function registerUser(){
+    const name = document.querySelector("input#name").value;
+    const password = document.querySelector("input#password").value;
+
+    console.log(name, password)
+
+
+
+}
