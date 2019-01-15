608886a0846741a594626c32419ccec1fcf96ee5
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-15 08:25:16 +0100

    finishedRegisteration

--- loginSource/register.js
+++ loginSource/register.js
@@ -1,9 +1,43 @@
+
+var userRegisteration;
+function init(){
+
+    //We initialize the database and put every single user in array
+    userRegisteration = [];
+    if (localStorage.userRegisterd) {
+        userRegisteration = localStorage.getObject("userRegisterd")
+
+    }
+}
+
+
 function registerUser(){
     const name = document.querySelector("input#name").value;
     const password = document.querySelector("input#password").value;
+    const user = {
+        name: name,
+        password: password
+    };
+   
+    // We check if the user alreay exist and if so then we stop registeration
 
-    console.log(name, password)
+    checkExistUser(name, password)
 
+    //If the user does not exist then the system will register it
+    userRegisteration.push(user)
+    localStorage.setObject("userRegisterd", userRegisteration);
 
+    
+  
 
+}
+
+function checkExistUser(name, password){
+    var usersRegistered = localStorage.getObject("userRegisterd");
+    usersRegistered.forEach(user => {
+    if(name == user.name && password == user.password){
+        alert("Du finns redan i systemet");
+        return;
+    }
+});
 }
