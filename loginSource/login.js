087c61d22f59ed18a065914d0248b47ee47772a3
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-24 21:28:04 +0100

    modfied

--- loginSource/login.js
+++ loginSource/login.js
@@ -5,34 +5,41 @@ function logIn(){
    
     
     var users = localStorage.getObject("userRegisterd");
-    users.forEach(user => {
-        
-        if(username == user.name && password == user.password){
-            signedIn = true;
-            return;  
-            
-        }
-    });
-    var order = [];
-    if(signedIn){
-        var shoppingCart = localStorage.getObject("shoppingCart");
-            
-        var user = localStorage.getObject("userRegisterd");
-        var addedOrder;
-        user.forEach(customer => {
-            
-            addedOrder = {
-                usersOrder: shoppingCart,
-                username: customer.name,
-                userId: Math.floor(Math.random() * 10),
-                orderDate: new Date()
-            }  
-        });
-        order.push(addedOrder);
-        var shoppingCart = localStorage.setObject("order", order);
+    if(!users){
+
+        alert("Du har inte registerat dig än")
+
     } else {
-        alert("Något gick fel")
+        users.forEach(user => {
+            
+            if(username == user.name && password == user.password){
+                signedIn = true;
+                return;  
+                
+            }
+        });
+        var order = [];
+        if(signedIn){
+            var shoppingCart = localStorage.getObject("shoppingCart");
+                
+            var user = localStorage.getObject("userRegisterd");
+            var addedOrder;
+            user.forEach(customer => {
+                
+                addedOrder = {
+                    usersOrder: shoppingCart,
+                    username: customer.name,
+                    userId: Math.floor(Math.random() * 10),
+                    orderDate: new Date()
+                }  
+            });
+            order.push(addedOrder);
+            var shoppingCart = localStorage.setObject("order", order);
+            window.location.href = "../index.html";
+        } else {
+            alert("Du har inte registerat dig än!")
+        }
+
     }
-    console.log(order)  
 }
 
