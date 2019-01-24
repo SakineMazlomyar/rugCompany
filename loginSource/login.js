5c5b578a2f6c1aa34463781547aa45d304620c58
SakineMazlomyar <mazlomyar12@gmail.com>
2019-01-24 15:03:15 +0100

    workingOnlogin

--- loginSource/login.js
+++ loginSource/login.js
@@ -1,16 +1,38 @@
 function logIn(){
+    var signedIn = undefined;
     const username = document.querySelector("input#username").value;
     const password = document.querySelector("input#password").value;
    
-
+    
     var users = localStorage.getObject("userRegisterd");
     users.forEach(user => {
-    if(username == user.name && password == user.password){
-        return window.location.assign('../index.html');
-    }
-    //Check we the not user does not work
-    if(username != user.name && password != user.password){
+        
+        if(username == user.name && password == user.password){
+            signedIn = true;
+            return;  
+            
+        }
+    });
+    var order = [];
+    if(signedIn){
+        var shoppingCart = localStorage.getObject("shoppingCart");
+            
+        var user = localStorage.getObject("userRegisterd");
+        var addedOrder;
+        user.forEach(customer => {
+            
+            addedOrder = {
+                usersOrder: shoppingCart,
+                username: customer.name,
+                userId: Math.floor(Math.random() * 10),
+                orderDate: new Date()
+            }  
+        });
+        order.push(addedOrder);
+        var shoppingCart = localStorage.setObject("order", order);
+    } else {
         alert("Något gick fel")
     }
-    });
+    console.log(order)  
 }
+
